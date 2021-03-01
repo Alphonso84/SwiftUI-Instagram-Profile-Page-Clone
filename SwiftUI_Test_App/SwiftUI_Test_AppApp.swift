9cbc2c03900fba578cce500965385de4050dac34
Alphonso Sensley II <alphonso.sensleyjr@gmail.com>
2021-03-01 14:02:20 -0800

    Initial Commit

---
+++ SwiftUI_Test_App/SwiftUI_Test_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Test_AppApp.swift
+//  SwiftUI_Test_App
+//
+//  Created by Alphonso Sensley II on 3/1/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Test_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
